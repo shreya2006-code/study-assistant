cb9c547cdded8cbb234c1cc1dae33822bc2ea4d0
shreya2006-code <ritaghosh014@gmail.com>
2026-05-23 21:03:51 +0530

    feat: completed day 3 part 1

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from app.core.settings import settings
+from app.db.database import engine
+from app.db.database import Base
+from app.models.user import User
 
-app = FastAPI()
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION
+)
+print("Database Connected Successfully!")
+
+Base.metadata.create_all(bind=engine)
+
+
 
 @app.get("/")
 def home():
-    return {"message": "Study Assistant Backend Running"}
+    return {
+        "message": f"{settings.APP_NAME} Running",
+        "debug": settings.DEBUG
+    }
